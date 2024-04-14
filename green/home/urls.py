de8e9b6a9f8e67f5ad7e731ca6502ad9ae7a8c9c
Geen-19 <91582806+Geen-19@users.noreply.github.com>
2024-04-14 11:37:45 +0000

    #3

--- green/home/urls.py
+++ green/home/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path("",views.index,name="home"),
+    path("home",views.index,name="home"),
+    path("about",views.index,name="about"),
     path("signup",views.signup,name="signup"),
     path("login",views.login,name="login"),
     path("user",views.user,name="user"),
